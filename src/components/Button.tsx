cecfd1fe782430df4684de5efcdac30dcee5b3f4
Wabi Jayme <wabihaime@gmail.com>
2024-10-28 23:37:48 +0800

    create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import classNames from "@/helpers/classnames";
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}
+
+export const Button = ({ children, onClick, className = "" }: ButtonProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className={classNames(
+        "flex items-center justify-center px-4 py-2",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
